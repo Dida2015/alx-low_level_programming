a67c93dd84548508711844f9914cdcbaebd76e0b
Dida2015 <emmanuelobare904@gmail.com>
2023-02-17 10:02:48 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+#include<stdio.h> /** *main - A program that prints a line with puts function v*Return: 0 (Success) */ int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0); }
